1bcb8afc130c774cedd93443afea55d8048d2154
Mike Skoe <rapidmajk@yandex.ru>
2021-07-21 18:20:57 +0700

    + item addition

--- main.ml
+++ main.ml
@@ -2,21 +2,27 @@ let (>>) f1 f2 arg = f2 (f1 arg)
 
 module Id = struct
   let counter = ref(0)
-      
+
   let of_string =
     String.to_seq
     >> Seq.map Char.code 
     >> List.of_seq 
     >> List.fold_left (+) 0 
-      
+
   let get_next () =
     let next_id = counter.contents + 1 in
     counter.contents <- next_id;
     next_id
-      
+
   type t =
     | Tag of int
     | Item of int 
+
+  let to_int = function
+    | Tag id -> id
+    | Item id -> id
+
+  let compare a b = (to_int a) - (to_int b)
 end 
 
 module Tag = struct
@@ -24,11 +30,13 @@ module Tag = struct
     id: Id.t;
     title: string;
   }
-  
+
   let make title = 
     let id = Id.(Tag(of_string title)) in
-    { id; title }
-end
+    { id; title } 
+
+  let compare a b = Id.(compare a.id b.id)
+end 
 
 module Item = struct
   type t = {
@@ -37,23 +45,35 @@ module Item = struct
     tags: string list;
     body: string;
   }
-  
+
   let make title tags body = 
     let id = Id.(Item (get_next ())) in
-    { id; title; tags; body }
+    { id; title; tags; body } 
+
+  let compare a b = Id.(compare a.id b.id)
 end
+
+module TagSet = Set.Make(Tag) 
+module ItemSet = Set.Make(Item)
 
 module Folder = struct
   type t = {
-    items: Item.t list;
-    tags: Tag.t list;
+    items: ItemSet.t;
+    tags: TagSet.t;
   }
-  
-  let make () = { items=[]; tags=[] } 
-  
+
+  let make () = { items=ItemSet.empty; tags=TagSet.empty } 
+
   let add_item (item: Item.t) t =
-    let tags = List.map Tag.make item.tags in
-    { items=item::t.items; tags=t.tags @ tags }
+    let tags =
+      item.tags
+      |> List.map Tag.make
+      |> List.fold_left (fun acc cur -> TagSet.add cur acc) TagSet.empty
+    in
+    { 
+      items=ItemSet.(add item t.items); 
+      tags=TagSet.(union t.tags tags);
+    }
 end
 
 let first_item = Item.make
