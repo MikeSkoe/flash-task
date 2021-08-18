85718995ce94c3c1ddf287e1d48385c4ef63ef12
Mike Skoe <rapidmajk@yandex.ru>
2021-08-18 14:16:39 +0700

    update Select

--- src/select/select.ml
+++ src/select/select.ml
@@ -1,34 +1,31 @@
 open Utils
 
-let memo ?is_equal:((=)=(=)) fn =
-      let last_arg_res = ref None in
-      (fun arg -> match !last_arg_res with
-      | Some (last_arg, last_res) when last_arg = arg ->
-            last_res
-      | _ ->
-            let new_res = fn arg in
-            last_arg_res := Some (arg, new_res);
-            new_res
-      )
+let memo ?is_equal:(is_eq=(=)) fn =
+  let last_arg_res = ref None in
+  (fun arg ->
+    match !last_arg_res with
+    | Some (last_arg, last_res) when is_eq last_arg arg -> last_res
+    | _ ->
+      let new_res = fn arg in
+      last_arg_res := Some (arg, new_res);
+      new_res
+  )
 
 type ('a, 'b) t = Select of ('a -> 'b)
+  
+let make fn = Select fn
+
+let return res = Select (fun _ -> res)
 
 let apply (Select fn) = fn
 
-let make ?is_equal:(is_equal=(=)) fn =
-      Select (memo ~is_equal fn)
-let map ?is_equal:(is_equal=(=)) (Select fn) map_fn =
-      Select (memo ~is_equal (fn >> map_fn))
-let map2 ?is_equal:(is_equal=(=)) (Select fn1) (Select fn2) map_fn =
-      Select (memo ~is_equal (fun arg ->
-            let a = fn1 arg in
-            let b = fn2 arg in
-            map_fn a b)
+let (>>=) (Select fn) bind_fn =
+      let memo_bind = memo bind_fn in
+      Select (fun arg ->
+            let new_val = fn arg in
+            let (Select fn) = memo_bind new_val in
+            fn arg
       )
-let if_map ?is_equal:(is_equal=(=)) (Select check) (Select on_true) (Select on_false) =
-      Select (memo ~is_equal (fun arg ->
-            if check arg
-            then on_true arg
-            else on_false arg
-      ))
-
+ 
+let branch (Select check) (Select if_true) (Select if_false) =
+      Select (fun arg -> if check arg then if_true arg else if_false arg)
