ba210b514935c421dbf3ea2d091c70d9ea66db14
Mike Skoe <rapidmajk@yandex.ru>
2021-08-18 14:17:03 +0700

    update select.mli

--- src/select/select.mli
+++ src/select/select.mli
@@ -1,24 +1,8 @@
-type ('a, 'b) t
-
-val apply:
-      ('a, 'b) t
-      -> ('a -> 'b)
-
-val make: ?is_equal:('a -> 'a -> bool)
-      -> ('a -> 'b)
-      -> ('a, 'b) t
-val map: ?is_equal:('a -> 'a -> bool)
-      -> ('a, 'b) t
-      -> ('b -> 'c)
-      -> ('a, 'c) t
-val map2: ?is_equal:('a -> 'a -> bool)
-      -> ('a, 'b) t
-      -> ('a, 'c) t
-      -> ('b -> 'c -> 'd)
-      -> ('a, 'd) t
-val if_map: ?is_equal:('a -> 'a -> bool)
-      -> ('a, bool) t
-      -> ('a, 'b) t
-      -> ('a, 'b) t
-      -> ('a, 'b) t
-
+  type ('a, 'b) t
+  
+  val make: ('a -> 'b) -> ('a, 'b) t
+  val return: 'b -> ('a, 'b) t
+  val apply: ('a, 'b) t -> 'a -> 'b
+  val (>>=): ('a, 'b) t -> ('b -> ('a, 'c) t) -> ('a, 'c) t
+  val branch: ('a, bool) t -> ('a, 'b) t -> ('a, 'b) t -> ('a, 'b) t
+  
