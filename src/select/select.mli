7fa2a4b529c590d0678811d30863902b4cc10a17
MikeSkoe <rapidmajk@yandex.ru>
2021-09-25 18:13:48 +0700

    move select lib to utils

--- src/select/select.mli
+++
@@ -1,8 +0,0 @@
-  type ('a, 'b) t
-  
-  val make: ('a -> 'b) -> ('a, 'b) t
-  val return: 'b -> ('a, 'b) t
-  val apply: ('a, 'b) t -> 'a -> 'b
-  val (>>=): ('a, 'b) t -> ('b -> ('a, 'c) t) -> ('a, 'c) t
-  val branch: ('a, bool) t -> ('a, 'b) t -> ('a, 'b) t -> ('a, 'b) t
-  
